aa9f5d22dcf51e2b99f81dce15ea7a881e104915
ppetrid <ppetrid@yawd.eu>
2012-10-07 13:50:55 +0300

    Updated Translatable.get_name() to accept the translation as argument, avoiding unnecessary queries (where applicable). Split get_name() in two functions so that get_name() handles the cache and _get_name() actually calculates the name. This allows for cleaner overrides (normaly one should only implement the _get_name() method).

--- translations/models.py
+++ translations/models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import clear_url_caches
 from django.db import models
 from django.db.models.signals import pre_delete, post_delete
-from django.utils import translation
+from django.utils.translation import get_language, get_language_info, ugettext as _
 from elfinder.fields import ElfinderField
 import utils
 
@@ -48,7 +48,7 @@ class Language(models.Model):
             super(Language, self).delete()
 
     def __unicode__(self):
-        return translation.get_language_info(self.name)['name']
+        return get_language_info(self.name)['name']
 
 class Translatable(models.Model):
     class Meta:
@@ -56,34 +56,45 @@ class Translatable(models.Model):
         
     def _get_cache_key(self, language):
         return '%s::%s::%s' % (self.__class__.__name__, self.id, language)
+    
+    def _get_name(self, language, translation):
+        """
+        Calculate the Translatable object's translation name.
+        """
+        try:
+            return unicode(self.translations.get(language_id=language) if not translation else translation)
+        except ObjectDoesNotExist:
+            try: #attempt to show default translation
+                return u'%s (%s %s)' % (unicode(self.translations.get(language__default=True)), translation.ugettext('not translated in'), language)
+            except ObjectDoesNotExist:
+                return u'%s #%s (%s %s)' % (self._meta.verbose_name, self.id, translation.ugettext('not translated in'), language)
             
-    def get_name(self, language=None):
-        #use current language, if not provided
+    def get_name(self, language=None, translation=None):
+        """
+        Get the Translatable object's display name for a given
+        language or translation.
+        """
+
+        #use the current language if not explicitly set
         if not language:
-            language = translation.get_language()
+            language = translation.language_id if translation else get_language()
             
         key = self._get_cache_key(language)
+
         name = cache.get(key)
 
         if not name: #not in cache
-            try:
-                name = self.translations.get(language_id=language).__unicode__()
-            except ObjectDoesNotExist:
-                try: #attempt to show default translation
-                    name = u'%s (%s %s)' % (self.translations.get(language__default=True).__unicode__(), translation.ugettext('not translated in'), language)
-                except ObjectDoesNotExist:
-                    name = u'%s #%s (%s %s)' % (self._meta.verbose_name, self.id, translation.ugettext('not translated in'), language)
+            name = self._get_name(language, translation)
             cache.set(key, name)
 
         return name
     
-    def update_name(self, name, language=None):
-        if not language:
-            language = translation.get_language()
-        cache.set(self._get_cache_key(language), name)
+    def update_name(self, language=None, translation=None):
+        cache.delete(self._get_cache_key(translation.language_id if translation else language))
+        self.get_name(language=language, translation=translation)
         
     def __unicode__(self):
-        return u'%s' % self.get_name()
+        return self.get_name()
         
 class Translation(models.Model):
     language = models.ForeignKey(Language)
@@ -98,13 +109,13 @@ class Translation(models.Model):
     def save(self):
         super(Translation, self).save()
         #update the display name for this language
-        self.translatable.update_name(self.name, self.language_id)
+        self.translatable.update_name(translation=self)
         
 def pre_delete_language(sender, instance, using, **kwargs):
     #admin actions make sure the default will not be deleted, so yawdcms does not directly need this
     #this is here to prevent 3rd party code from accidentily deleting the default language  
     if instance.default:
-        raise Exception(translation.ugettext("Cannot delete the default language"))
+        raise Exception(_("Cannot delete the default language"))
 
 def post_delete_language(sender, instance, using, **kwargs):
     """
