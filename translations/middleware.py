aa54f2490cde93b29b23060589dd20adb17c7f8a
ppetrid <ppetrid@yawd.eu>
2012-11-12 16:57:59 +0200

    translations middleware to accepth i18n_patterns as well

--- translations/middleware.py
+++ translations/middleware.py
@@ -54,19 +54,9 @@ class TranslationMiddleware(LocaleMiddleware):
         
         #original django middleware used to redirect 404 responses to a 
         #language-dependend url if language prefix patterns were used  
-        #this behiavior is removed from yawdcms
+        #this behavior is removed from yawd-translations
          
         patch_vary_headers(response, ('Accept-Language',))
         if 'Content-Language' not in response:
             response['Content-Language'] = language
         return response
-    
-    def is_language_prefix_patterns_used(self):
-        """
-        Returns `True` if the `LocaleRegexURLResolver` is used
-        at root level of the urlpatterns, else it returns `False`.
-        """
-        for url_pattern in get_resolver(None).url_patterns:
-            if isinstance(url_pattern, TranslationRegexURLResolver):
-                return True
-        return False
