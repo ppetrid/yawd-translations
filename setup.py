e01286df6e734aa01cbc0ebe358c8e7044fd11c0
ppetrid <ppetrid@yawd.eu>
2013-03-06 21:42:16 +0200

    require yawd-elfinder from master

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
         ],
       include_package_data = True,
       install_requires = [
-        "yawd-elfinder>=0.90.02",
+        "git+git://github.com/yawd/yawd-elfinder.git@master#egg=yawd-elfinder",
         ],
 )
