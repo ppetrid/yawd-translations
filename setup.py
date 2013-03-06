e7a89a447d265aad5823ecc71353ec683ca69181
ppetrid <ppetrid@yawd.eu>
2013-03-06 15:11:50 +0200

    added long description

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
       url='http://yawd.eu/open-source-projects/yawd-translations/',
       version = translations.__version__,
       description='A set of tools for creating multilingual websites with Django',
+      long_description=open('README.rst', 'rt').read(),
       author='yawd',
       author_email='info@yawd.eu',
       packages=find_packages(),
