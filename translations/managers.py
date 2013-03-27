7673230eaea0d1f8eb85fd81ebaf5fe08bc1c8e7
ppetrid <ppetrid@yawd.eu>
2013-03-27 16:20:43 +0200

    minor docstring update

--- translations/managers.py
+++ translations/managers.py
@@ -5,9 +5,6 @@ class TranslatableManager(models.Manager):
     Override the default manager for
     :class:`translations.models.Translatable` objects to prefetch
     the related :class:`translations.models.Translation` translations.
-    
-    This way we can exploit the queryset cache to minimize database
-    hits. 
     """
     use_for_related_fields = True
     
